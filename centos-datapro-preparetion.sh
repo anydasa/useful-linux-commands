feca20ac233b38633a76334752ba46de7ddbedbf
anydasa <anydasa@gmail.com>
2017-09-12 19:17:34 +0300

    centos-datapro-preparetion

---
+++ centos-datapro-preparetion.sh
@@ -0,0 +1,8 @@
+yum -y install git
+yum check-update
+curl -fsSL https://get.docker.com/ | sh
+systemctl start docker
+systemctl enable docker
+sudo usermod -aG docker $(whoami)
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
